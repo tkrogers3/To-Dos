5593be46209f49d9259652c25ccaf4bfb52394d5
tkrogers3 <tkrogers3@gmail.com>
2020-03-26 20:12:11 -0400

    Updates

---
+++ precache-manifest.8e822d1122d2a46c51f51a97a0d913d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0a8acc1b40c4f06df4307930ce6de4",
+    "url": "/To-Dos/index.html"
+  },
+  {
+    "revision": "f2b7a3b55ce3030dc4ad",
+    "url": "/To-Dos/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "652f8e6c5739286ac4d3",
+    "url": "/To-Dos/static/css/main.a3d1fb7e.chunk.css"
+  },
+  {
+    "revision": "f2b7a3b55ce3030dc4ad",
+    "url": "/To-Dos/static/js/2.570e266f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Dos/static/js/2.570e266f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652f8e6c5739286ac4d3",
+    "url": "/To-Dos/static/js/main.119515d2.chunk.js"
+  },
+  {
+    "revision": "bd293cca381ac34d0734",
+    "url": "/To-Dos/static/js/runtime-main.086be2fa.js"
+  },
+  {
+    "revision": "bb11331879b13cdf6b4d0687f2ee53f9",
+    "url": "/To-Dos/static/media/coark.bb113318.jpg"
+  }
+]);
